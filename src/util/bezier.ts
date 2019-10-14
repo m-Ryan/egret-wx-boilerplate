a66e2abc3234fa728fdda01a54a189768a6e2a9c
m-Ryan <962491243@qq.com>
2019-10-14 23:09:08 +0800

    svg-parse

--- src/util/bezier.ts
+++ src/util/bezier.ts
@@ -22,3 +22,44 @@ function QuadraticBezier(p0: BezierPoint, p1: BezierPoint, p2: BezierPoint, t: n
         y
     };
 }
+
+function parseSvgPath(d: string) {
+  let paths = d.split(' ');
+  let pattern = 'MLCQAZ';
+
+  const parseData = [];
+  while (paths.length > 0) {
+      const current = paths.splice(0, 1)[0];
+      const char = current.charAt(0).toUpperCase();
+      if (char === 'Z') {
+        parseData.push({
+          type: 'Z',
+          values: []
+        });
+        break;
+      }
+      if (pattern.indexOf(char) !== -1) {
+        const data = {
+          type: char,
+          values: [checkValidPoint(current.substring(1))]
+        };
+
+        while(paths.length > 0 && (pattern.indexOf(paths[0].charAt(0).toUpperCase()) === -1)) {
+          data.values.push(checkValidPoint(paths.splice(0, 1)[0]));
+        }
+        parseData.push(data);
+      }
+
+  }
+  return parseData;
+  
+};
+
+function checkValidPoint(point: string) {
+  if (point === ''|| typeof Number(point) !== 'number') {
+    throw new Error('svg path 错误');
+  }
+  return point;
+}
+
+console.log(parseSvgPath("M150 0 L75 200 L225 20 q150 -300 300 0 a75 75 0 1 0 150 0 L225 200 Z"))
